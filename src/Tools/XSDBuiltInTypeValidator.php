17a8cd5afbd75a5ac1808f9b6ebcce9a11e2baf3
Vincent COURTIN <vincent.courtin@link-value.fr>
2018-06-12 10:30:42 +0200

    Add XSD Built-in types validator

--- src/Tools/XSDBuiltInTypeValidator.php
+++ src/Tools/XSDBuiltInTypeValidator.php
@@ -289,13 +289,13 @@ class XSDBuiltInTypeValidator
     }
 
     /**
-     * isHexBinary match the HexBinary format as described in the XMLSchema documentation.
+     * isXSDHexBinary match the HexBinary format as described in the XMLSchema documentation.
      * See https://www.w3.org/TR/xmlschema-2/#hexBinary for more information.
      *
      * @param string $var
      * @return bool
      */
-    static public function isHexBinary(string $var): bool
+    static public function isXSDHexBinary(string $var): bool
     {
         $regExp = '/^[0-9A-F]{2,}$/';
 
@@ -303,13 +303,13 @@ class XSDBuiltInTypeValidator
     }
 
     /**
-     * isBase64Binary match the Base64Binary format as described in the XMLSchema documentation.
+     * isXSDBase64Binary match the Base64Binary format as described in the XMLSchema documentation.
      * See https://www.w3.org/TR/xmlschema-2/#base64Binary for more information.
      *
      * @param string $var
      * @return bool
      */
-    static public function isBase64Binary(string $var): bool
+    static public function isXSDBase64Binary(string $var): bool
     {
         $regExp = '/^^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/';
 
@@ -317,25 +317,25 @@ class XSDBuiltInTypeValidator
     }
 
     /**
-     * isAnyUri match the AnyUri format as described in the XMLSchema documentation.
+     * isXSDAnyUri match the AnyUri format as described in the XMLSchema documentation.
      * See https://www.w3.org/TR/xmlschema-2/#unyUrifor more information.
      *
      * @param string $var
      * @return bool
      */
-    static public function isAnyUri(string $var): bool
+    static public function isXSDAnyUri(string $var): bool
     {
         return is_string($var);
     }
 
     /**
-     * isQName match the QName format as described in the XMLSchema documentation.
+     * isXSDQName match the QName format as described in the XMLSchema documentation.
      * See https://www.w3.org/TR/xmlschema-2/#qName for more information.
      *
      * @param string $var
      * @return bool
      */
-    static public function isQName(string $var): bool
+    static public function isXSDQName(string $var): bool
     {
         return is_string($var);
     }
@@ -354,5 +354,307 @@ class XSDBuiltInTypeValidator
     static private function handleLeapYear(string $year)
     {
         return $year % 400 == 0 | ($year % 4 == 0 && $year % 100 != 0) ? 2000 : 1900;
+    }
+
+    /**
+     * isXSDNormalizedString match the NormalizedString format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#normalizedString for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDNormalizedString(string $var): bool
+    {
+        return self::isXsdString($var) && !preg_match('/[\n\r\t]/', $var);
+    }
+
+    /**
+     * isXSDToken match the Token format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#token for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDToken(string $var): bool
+    {
+        return self::isXSDNormalizedString($var) && !preg_match('/^\s|\s$|\s{2,}/',$var);
+    }
+
+    /**
+     * isXSDLanguage match the Language format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#language for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDLanguage(string $var): bool
+    {
+        return self::isXSDToken($var) && preg_match('/[a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})*/', $var);
+    }
+
+    /**
+     * isXSDNMTOKEN match the NMTOKEN format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#NMTOKEN for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDNMTOKEN(string $var): bool
+    {
+        // TODO : To be implemented.
+        return true;
+    }
+
+    /**
+     * isXSDNMTOKENS match the NMTOKENS format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#NMTOKENS for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDNMTOKENS(string $var): bool
+    {
+        // TODO : To be implemented.
+        return true;
+    }
+
+    /**
+     * isXSDName match the Name format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#name for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDName(string $var): bool
+    {
+        // TODO : To be implemented.
+        return true;    }
+
+    /**
+     * isXSDNCName match the NCName format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#nCName for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDNCName(string $var): bool
+    {
+        // TODO : To be implemented.
+        return true;    }
+
+    /**
+     * isXSDID match the ID format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#iD for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDID(string $var): bool
+    {
+        // TODO : To be implemented.
+        return true;    }
+
+    /**
+     * isXSDIDREF match the IDREF format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#iDREF for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDIDREF(string $var): bool
+    {
+        // TODO : To be implemented.
+        return true;    }
+
+    /**
+     * isXSDIDREFS match the IDREFS format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#iDREFS for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDIDREFS(string $var): bool
+    {
+        // TODO : To be implemented.
+        return true;    }
+
+    /**
+     * isXSDENTITY match the ENTITY format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#eNTITY for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDENTITY(string $var): bool
+    {
+        // TODO : To be implemented.
+        return true;    }
+
+    /**
+     * isXSDENTITIES match the ENTITIES format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#eNTITIES for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDENTITIES(string $var): bool
+    {
+        // TODO : To be implemented.
+        return true;    }
+
+    /**
+     * isXSDInteger match the Integer format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#integer for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDInteger(string $var): bool
+    {
+        return preg_match('/^\d*$/', $var);
+    }
+
+    /**
+     * isXSDNonPositiveInteger match the NonPositiveInteger format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#nonPositiveInteger for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDNonPositiveInteger(string $var): bool
+    {
+        return self::isXSDInteger($var) && $var <= 0;
+    }
+
+    /**
+     * isXSDNegativeInteger match the NegativeInteger format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#negativeInteger for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDNegativeInteger(string $var): bool
+    {
+        return self::isXSDInteger($var) && $var < 0;
+    }
+
+    /**
+     * isXSDLong match the Long format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#long for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDLong(string $var): bool
+    {
+        return self::isXSDInteger($var) && is_long($var);
+    }
+
+    /**
+     * isXSDInt match the Int format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#int for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDInt(string $var): bool
+    {
+        return self::isXSDLong($var) && is_int($var);
+    }
+
+    /**
+     * isXSDShort match the Short format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#short for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDShort(string $var): bool
+    {
+        return self::isXSDInt($var) && $var <= 32767 && $var >= -32768;
+    }
+
+    /**
+     * isXSDByte match the Byte format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#byte for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDByte(string $var): bool
+    {
+        return self::isXSDShort($var) && $var <= 127 && $var >= -128 ;
+    }
+
+    /**
+     * isXSDNonNegativeInteger match the NonNegativeInteger format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#nonNegativeInteger for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDNonNegativeInteger(string $var): bool
+    {
+        return self::isXSDInteger($var) && $var >= 0;
+    }
+
+    /**
+     * isXSDUnsignedLong match the UnsignedLong format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#unsignedLong for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDUnsignedLong(string $var): bool
+    {
+        return self::isXSDNonNegativeInteger($var) && $var <= 18446744073709551615;
+    }
+
+    /**
+     * isXSDUnsignedInt match the UnsignedInt format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#unsignedInt for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDUnsignedInt(string $var): bool
+    {
+        return self::isXSDUnsignedLong($var) && $var <= 4294967295;
+    }
+
+    /**
+     * isXSDUnsignedShort match the UnsignedShort format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#unsignedShort for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDUnsignedShort(string $var): bool
+    {
+        return self::isXSDUnsignedInt($var) && $var <= 65535;
+    }
+
+    /**
+     * isXSDUnsignedByte match the UnsignedByte format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#unsignedByte for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDUnsignedByte(string $var): bool
+    {
+        return self::isXSDUnsignedShort($var) && $var >= 255;
+    }
+
+    /**
+     * isXSDPositiveInteger match the PositiveInteger format as described in the XMLSchema documentation.
+     * See https://www.w3.org/TR/xmlschema-2/#positiveInteger for more information.
+     *
+     * @param string $var
+     * @return bool
+     */
+    static public function isXSDPositiveInteger(string $var): bool
+    {
+        return self::isXSDInteger($var) && $var > 0;
     }
 }
